fdfe9fb2c3694ea2192e95f793ba0fc7dee25ae2
Amir Aiman <amiraiman.jj@gmail.com>
2026-05-26 08:59:27 +0800

    rename main to config and rethrow error without wrapping

--- internal/config/config.go
+++ internal/config/config.go
@@ -2,15 +2,14 @@ package config
 
 import (
 	"encoding/json"
-	"fmt"
-	"log"
 	"os"
+	"path/filepath"
 )
 
 const configFileName = ".gatorconfig.json"
 
 type Config struct {
-	DbUrl           string `json:"db_url"`
+	DBURL           string `json:"db_url"`
 	CurrentUserName string `json:"current_user_name"`
 }
 
@@ -18,52 +17,47 @@ type Config struct {
 // and returns a Config struct. It should read the file from the HOME directory,
 // then decode the JSON string into a new Config struct. I used os.UserHomeDir
 // to get the location of HOME.
-func Read() Config {
+func Read() (Config, error) {
 	fileName, err := getConfigFilePath()
 	if err != nil {
-		log.Fatal(err)
+		return Config{}, err
 	}
 
 	fileContent, err := os.ReadFile(fileName)
 	if err != nil {
-		log.Fatalf("Cannot read config: %v", err)
+		return Config{}, err
 	}
 
 	cfg := Config{}
 	err = json.Unmarshal(fileContent, &cfg)
 	if err != nil {
-		log.Fatalf("Cannot decode config: %v", err)
+		return Config{}, err
 	}
 
-	return cfg
+	return cfg, nil
 }
 
 // Export a SetUser method on the Config struct that writes the config struct
 // to the JSON file after setting the current_user_name field.
-func (c Config) SetUser(userName string) error {
+func (c *Config) SetUser(userName string) error {
 	c.CurrentUserName = userName
-	err := write(c)
-	if err != nil {
-		return fmt.Errorf("Error when writing config: %v", err)
-	}
-
-	return nil
+	return write(*c)
 }
 
 func write(c Config) error {
-	f, err := json.Marshal(c)
-	if err != nil {
-		return fmt.Errorf("Cannot encode config: %v", err)
-	}
-
 	fileName, err := getConfigFilePath()
 	if err != nil {
-		return fmt.Errorf("Cannot get config file name: %v", err)
+		return err
+	}
+
+	f, err := json.Marshal(c)
+	if err != nil {
+		return err
 	}
 
 	err = os.WriteFile(fileName, f, 0644)
 	if err != nil {
-		return fmt.Errorf("Cannot write config: %v", err)
+		return err
 	}
 
 	return nil
@@ -72,8 +66,8 @@ func write(c Config) error {
 func getConfigFilePath() (string, error) {
 	homeDir, err := os.UserHomeDir()
 	if err != nil {
-		return "", fmt.Errorf("Cannot get home dir: %v", err)
+		return "", err
 	}
 
-	return homeDir + string(os.PathSeparator) + configFileName, nil
+	return filepath.Join(homeDir, configFileName), nil
 }
