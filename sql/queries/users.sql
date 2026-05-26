72c6f2e5f5dec47879ffe8f7ccf1e9f5be9b9b39
Amir Aiman <amiraiman.jj@gmail.com>
2026-05-26 16:35:56 +0800

    add sqlc user schema & queries

---
+++ sql/queries/users.sql
@@ -0,0 +1,17 @@
+-- name: GetUser :one
+SELECT * FROM users
+WHERE name = $1 LIMIT 1;
+
+-- name: ListUsers :many
+SELECT * FROM users
+ORDER BY name;
+
+-- name: CreateUser :one
+INSERT INTO users (id, created_at, updated_at, name)
+VALUES (
+    $1,
+    $2,
+    $3,
+    $4
+)
+RETURNING *;
